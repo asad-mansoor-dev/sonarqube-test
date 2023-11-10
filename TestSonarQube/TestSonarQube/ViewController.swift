40ce85d8051484379ca41abddbbc3970755a9cc5
Asad Mehmood <asadmehmood@Asad-Mehmood.local>
2023-11-10 16:39:08 +0500

    initial commit

---
+++ TestSonarQube/TestSonarQube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSonarQube
+//
+//  Created by Asad Mehmood on 10/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
